b29be9e5960d9fc3110a8ef4f71d146848300bea
Dieter <odzdit@me.com>
2016-09-10 15:39:33 -0400

    working dashboard through controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,24 +1,30 @@
 class StaticPagesController < ApplicationController
 	
 	def dashboard
-				@data = {
-		  labels: ["January", "February", "March", "April", "May", "June", "July"],
-		  datasets: [
-		    {
-		        label: "My First dataset",
-		        backgroundColor: "rgba(220,220,220,0.2)",
-		        borderColor: "rgba(220,220,220,1)",
-		        data: [65, 59, 80, 81, 56, 55, 40]
-		    },
-		    {
-		        label: "My Second dataset",
-		        backgroundColor: "rgba(151,187,205,0.2)",
-		        borderColor: "rgba(151,187,205,1)",
-		        data: [28, 48, 40, 19, 86, 27, 90]
-		    }
-		  ]
-		}
-		# options = { ... }
+			 @data = {
+              labels: ["January", "February", "March", "April", "May", "June", "July"],
+              datasets: [
+                {
+                    label: "My First dataset",
+                    backgroundColor: "rgba(220,220,220,0.2)",
+                    borderColor: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "My Second dataset",
+                    backgroundColor: "rgba(151,187,205,0.2)",
+                    borderColor: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                },
+                { 
+                    label: "My Third dataset",
+                    backgroundColor: "rgba(151,187,205,0.2)",
+                    borderColor: "rgba(151,187,205,1)",
+                    data: [20, 20, 40, 20, 20, 20, 90]
+                }
+              ]
+            } 
+            @options = {:width => 800, :height => 300}             
 	end
 
 end
