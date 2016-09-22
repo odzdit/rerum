22a189ab4c86b56c530cf46af144ef75f4ef1d88
Dieter Ordonez <odzdit@me.com>
2016-09-22 01:24:24 -0400

    Created the Login Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 root to: 'static_pages#home'
 get '/dashboard' => 'dashboard#dashboard'
+get "login/prompt"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
