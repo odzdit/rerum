a44093c97e5897b674985888c89fb2b6a74c1cb5
Dieter <odzdit@me.com>
2016-10-06 23:16:49 -0400

    did more changes to the homepage & modals

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -11,4 +11,16 @@ $(document).ready(function(){
 		$(".js-glyphicon-table-modal").modal("show")
 		// console.log(second_array_names)
 	})
+
+	$(".js-glyphicon-time").on("click", function(){
+		
+		$(".js-glyphicon-time-modal").modal("show")
+		// console.log(second_array_names)
+	})
+
+	$(".js-glyphicon-circle").on("click", function(){
+		
+		$(".js-glyphicon-circle-modal").modal("show")
+		// console.log(second_array_names)
+	})
 });
