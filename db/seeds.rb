6c5974cbe35027470e726957065a9ca74d14dcdc
Dieter <odzdit@me.com>
2016-10-10 18:15:45 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,4 @@
 User.create(name: 'Javier Meza', email: "info@cancunrivieramaya.com", password: "rerum123", password_confirmation: "rerum123", adwords_id: "140-581-8720")
 User.create(name: 'Isaac Sitton', email: "sittonsutton@prodigy.mx", password: "rerum123", password_confirmation: "rerum123", adwords_id: "171-089-7853")
 User.create(name: 'Mau Diaz', email: "mau@leco.mx", password: "rerum123", password_confirmation: "rerum123", adwords_id: "613-715-8320")
+User.create(name: 'Dieter', email: "odzdit@me.com", password: "rerum123", password_confirmation: "rerum123", adwords_id: "451-846-4440")
